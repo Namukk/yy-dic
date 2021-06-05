481ad65a8734c66fb32fdf3336f612eff75bf101
Namukk <skadnr625@gmail.com>
2021-06-05 05:39:04 +0300

    AUTHFORM

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Router from "./components/Router";
+import { authService } from "./fbase";
 
 function App() {
-  return <Router />;
+  const [init, setInit] = useState(false);
+  const [userObj, setUserObj] = useState(null);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid.refreshUser,
+          updateProfile: (args) => user.updateProfile(args),
+        });
+        // setIsLoggedIn(true);
+        setUserObj(user);
+      } else {
+        setUserObj(null);
+      }
+      setInit(true);
+    });
+  }, []);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid.refreshUser,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
+  return (
+    <>
+      {init ? (
+        <Router
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+          refreshUser={refreshUser}
+        />
+      ) : (
+        "Initializing..."
+      )}
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,68 @@
+import { authService, dbService } from "../fbase";
+import React, { useEffect, useState } from "react";
+// import { useHistory } from "react-router-dom";
+
+const Profile = ({ userObj, refreshUser }) => {
+  //   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+  const onLogOutClick = () => {
+    authService.signOut();
+    // history.push("/"); 6번과 10번줄을 사용해도 라우터에서 사용한 <Redirect> 태그와 같은 효과. 로그아웃해도 프로필에 남아있는 현상 해결 가능.
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    }
+  };
+
+  //   const getMyNweets = async () => {
+  //     const ynweets = await dbService
+  //       .collection("Ynweets")
+  //       .where("creatorId", "==", userObj.uid)
+  //       .orderBy("createdAt")
+  //       .get();
+  //     console.log(ynweets.docs.map((doc) => doc.data()));
+  //   };
+
+  //   useEffect(() => {
+  //     getMyNweets();
+  //   }, []);
+  return (
+    <div className="container">
+      <form onSubmit={onSubmit} className="profileForm">
+        <input
+          onChange={onChange}
+          type="text"
+          autoFocus
+          placeholder="Display name"
+          value={newDisplayName}
+          className="formInput"
+        />
+        <input
+          type="submit"
+          value="Update Profile"
+          className="formBtn"
+          style={{
+            marginTop: 10,
+          }}
+        />
+      </form>
+      <span className="formBtn cancelBtn logOut" onClick={onLogOutClick}>
+        Log Out
+      </span>
+    </div>
+  );
+};
+
+export default Profile;
